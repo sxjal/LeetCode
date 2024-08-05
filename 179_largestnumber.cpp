b885e88b116a2d11e970976fca4c2f9593d47a11
sajal <sajalsahu8@gmail.com>
2024-08-06 02:56:08 +0530

    updated

---
+++ 179_largestnumber.cpp
@@ -0,0 +1,49 @@
+
+#include<iostream>
+#include<vector>
+#include <string>
+
+using namespace std;
+
+
+#include<iostream>
+#include<vector>
+#include <string>
+
+using namespace std;
+
+int func(string s, int i, int j){
+    int count = 0;
+
+
+    //jabtak match karega tab kar count increment and i-- and j++
+    while(i>= 0 && j < s.length() && s[i] == s[j]){
+        count++;
+        i--;
+        j++;
+    }
+    
+    return count;
+}
+
+int countSubstrings(string s) {
+    int count = 0;
+
+    for(int i = 0; i < s.size(); i++){
+        count += func(s,i,i); //odd ans
+        count += func(s,i,i+1); //even ans
+    }
+
+    return count;
+}
+int main()
+{
+   string s = "noon";
+
+
+    cout<<countSubstrings(s);
+    return 0;
+}
+
+
+
